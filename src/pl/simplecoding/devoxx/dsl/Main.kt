55f3d83de379c8d809527c1a72e7a46231b224e8
ArturC <artur.m.czopek@metrosoft.com>
2018-05-30 20:54:51 +0100

    7. DSL - added more battles

--- src/pl/simplecoding/devoxx/dsl/Main.kt
+++ src/pl/simplecoding/devoxx/dsl/Main.kt
@@ -25,6 +25,61 @@ fun main(args: Array<String>) {
                     "Not Found" power 40
                 }
             }
+            wizard("Gandalf") {
+                level = 22
+                equipment {
+                    "Rod" power 12
+                }
+                spells {
+                    "BOOOM!" power 80
+                }
+            } versus wizard("Saruman") {
+                level = 22
+                equipment {
+                    "Rod" power 12
+                }
+                spells {
+                    "BOOOM!" power 80
+                }
+            }
+            knight("Black") {
+                level = 14
+                equipment {
+                    "Sword" power 40
+                    "Shield" power 30
+                    "Nike Air Max" power 40
+                }
+                spells {
+                    "Wowowowow" power 14
+                }
+            } versus knight("White") {
+                level = 23
+                equipment {
+                    "Sword" power 11
+                    "Shield" power 60
+                    "Nike Air Max" power 15
+                }
+                spells {
+                    "Select * from nothing" power 4
+                }
+            }
+            knight("Iron Man") {
+                level = 17
+                equipment {
+                    "Sword" power 20
+                    "Shield" power 5
+                    "Nike Air Max" power 40
+                }
+            } versus wizard("Pikachu") {
+                level = 19
+                equipment {
+                    "Rod" power 22
+                }
+                spells {
+                    "Vingardium Leviosa" power 30
+                    "Ebebebebe" power 30
+                }
+            }
         }
     }
 
