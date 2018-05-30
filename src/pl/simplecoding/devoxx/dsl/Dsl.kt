33e9a8c188db50f8184ae628b29820fa370a4fc2
ArturC <artur.m.czopek@metrosoft.com>
2018-05-30 20:49:19 +0100

    6. DSL - FINISHED DSL, APPLIED TO WHOLE TOURNAMENT

--- src/pl/simplecoding/devoxx/dsl/Dsl.kt
+++ src/pl/simplecoding/devoxx/dsl/Dsl.kt
@@ -2,6 +2,18 @@ package pl.simplecoding.devoxx.dsl
 
 fun tournament(block: TournamentBuilder.() -> Unit): Tournament = TournamentBuilder().apply(block).build()
 
+fun wizard(name: String, block: HeroBuilder.() -> Unit) = HeroBuilder()
+        .apply {
+            this.name = name
+            this.profession = Profession.WIZARD
+        }.apply(block).build()
+
+fun knight(name: String, block: HeroBuilder.() -> Unit) = HeroBuilder()
+        .apply {
+            this.name = name
+            this.profession = Profession.KNIGHT
+        }.apply(block).build()
+
 class TournamentBuilder {
     var name: String = ""
 
@@ -16,4 +28,30 @@ class TournamentBuilder {
 
 class Battles: ArrayList<Battle>() {
     infix fun Hero.versus(opponent: Hero) = add(Battle(this, opponent))
+}
+
+class HeroBuilder {
+    var name = ""
+    var profession = Profession.WIZARD
+    var level = 0
+    var equipment = mutableListOf<Item>()
+    var spells = mutableListOf<Spell>()
+
+    fun equipment(block: Items.() -> Unit) {
+        equipment.addAll(Items().apply(block))
+    }
+
+    fun spells(block: Spells.() -> Unit) {
+        spells.addAll(Spells().apply(block))
+    }
+
+    fun build() = Hero(name, profession, level, equipment, spells)
+}
+
+class Items: ArrayList<Item>() {
+    infix fun String.power(power: Int) = add(Item(this, power))
+}
+
+class Spells: ArrayList<Spell>() {
+    infix fun String.power(power: Int) = add(Spell(this, power))
 }
